aa5aeba19a82afdbe8304f76e6c0a1d2c1cd717b
Invectys <34795455+Invectys@users.noreply.github.com>
2020-12-29 14:53:52 +0300

    add

---
+++ BlazorClient/BlazorClient/TestImages.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClient
+{
+    public class TestImages
+    {
+
+        public List<byte[]> Loaded => _images;
+
+        private List<byte[]> _images = new List<byte[]>();
+
+        private int _lastImageInd = 9;
+        private HttpClient _client;
+
+        public TestImages(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task LoadImages()
+        {
+            
+            for(int i = 1; i <= _lastImageInd; i++)
+            {
+                var result = await _client.GetAsync("/TestImages/" + i + ".jpg");
+                var bytes = await result.Content.ReadAsByteArrayAsync();
+                _images.Add(bytes);
+            }
+
+        }
+
+
+    }
+}
---
+++ BlazorClient/BlazorGallery/Modules/GalleryMaker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorGallery
+{
+    public class GalleryMaker
+    {
+        public int ColumnAmount { get; set; }
+        public List<byte[]> Images => _bytes;
+
+        private List<byte[]> _bytes = new List<byte[]>();
+
+
+        public GalleryMaker(int columnAmount = 3)
+        {
+            ColumnAmount = columnAmount;
+        }
+
+        public void SetImages(List<byte[]> bytes)
+        {
+            _bytes = bytes;
+        }
+       
+
+
+    }
+}
